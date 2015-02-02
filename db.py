d482835d6ed0c15d0c7c2d350a51acb6565a5e83
Rbukharov <roman.buharov@gmail.com>
2015-02-02 23:30:15 +0300

    DB class implemented

---
+++ db.py
@@ -0,0 +1,137 @@
+from peewee import *
+from time import gmtime, strftime
+import uuid
+import os
+
+# Путь к БДы
+path = 'db\\'
+
+# БД параметров
+settings_db = SqliteDatabase(path + 'settings.db')
+
+# основные параметры
+parameters = [   { 'name': 'bamboo_token',     'value': ' '},
+				 { 'name': 'jira_token',       'value': ' '},
+				 { 'name': 'confluence_token', 'value': ' '},
+				 { 'name': 'bamboo_url',       'value': 'https://build.itvgroup.ru/bamboo/rest/api/latest/'},   # url api build сервера
+				 { 'name': 'jira_url',         'value': 'https://support.axxonsoft.com/jira/rest/api/latest/'},   # url api jira
+				 { 'name': 'confluence_url',   'value': 'https://doc.axxonsoft.com/confluence/rest/api/latest/'}    # url api confluence
+				 ]
+#________________ схема данных ________________
+
+# таблица основных параметров
+class MainSettings(Model):
+	name  = CharField(default = "")
+	value = CharField(default = "")
+	class Meta:
+		database = settings_db
+
+# таблица параметров раздела weekly builds
+class WeeklyBuilds(Model):
+	#owner      = ForeignKeyField(Monitor_type, related_name='monitors') # тип
+	name             = CharField(default = "") # название сборки
+	bamboo_plan      = CharField(default = "") # план build сервера
+	jira_filters     = CharField(default = "") # фильтры jira (в формате json)
+	confluence_page  = CharField(default = "") # страница confluence
+	class Meta:
+		database = settings_db
+
+#________________ инициализация базы данных ________________
+
+# операции с БД параметров
+class SettingsDB(object):
+	def __init__(self):
+		# создание БД и ее схемы, если ее нет
+		try: # создание таблиц
+			WeeklyBuilds.create_table()
+			MainSettings.create_table()
+			print("Settings database has been created")
+		except:
+			print("Settings database schema loaded")
+		# инициализация основлных параметров
+		for p in parameters:
+			try:
+				MainSettings.get(MainSettings.name == p['name'])
+			except:
+				parameter = MainSettings.create(name = p['name'], value = p['value'])
+				parameter.save()
+
+		# инициализация паарметров сборок
+		try:
+			WeeklyBuilds.get(WeeklyBuilds.name == "Axxon Next v.3.6.3")
+		except:
+			build = WeeklyBuilds.create(name = "Axxon Next v.3.6.3")
+			build.save()
+
+##________________ операции с БД ________________
+
+	# редактирование основных параметров
+	# создавать и удалять их нельзя
+	def editMainSettings(self, name, value):
+		if(name != None and value != None):
+			parameter = MainSettings.get(MainSettings.name == name)
+			parameter.value = value
+			if(parameter.save()):
+				print("Parameter " + name + " updated")
+				return True
+			else:
+				return False
+		else:
+			return False
+	# новая сборка
+	def createBuild(self, name, plan, filters, page):
+		if(name == None):
+			_name = "New build"
+		else:
+			_name = name
+		if(plan != None):
+			WeeklyBuilds.create(name            = _name,
+								bamboo_plan     = plan,
+								jira_filters    = filters,
+								confluence_page = page)
+			print("Build " + _name + " has been created")
+			return True
+		else:
+			return False
+
+	# обновление сборки
+	def updateBuild(self, id = None, name = None, plan = None, filters = None, page = None):
+		if(id != None):
+			try:
+				build = WeeklyBuilds.get(WeeklyBuilds.id == id)
+			except:
+				return False
+			if(name != None):
+				build.name = name
+			if(plan != None):
+				build.bamboo_plan = plan
+			if(filters != None):
+				build.jira_filters = filters
+			if(name != None):
+				build.confluence_page = page
+			if(build.save()):
+				return True
+			else:
+				return False
+		else:
+			return False
+
+	# удаление сборки
+	def removeBuild(self, id):
+		if(id != None):
+			try:
+				build = WeeklyBuilds.get(WeeklyBuilds.id == id)
+				build.delete_instance()
+				return True
+			except:
+				return False
+		else:
+			return False
+
+	# получить список созданных сборок
+	def getBuilds(self):
+		builds = []
+		for b in WeeklyBuilds.select():
+			build = {'id': b.id, 'name': b.name, 'bamboo_plan': b.bamboo_plan, 'jira_filters': b.jira_filters, 'confluence_page': b.confluence_page}
+			builds.append(build)
+		return builds
